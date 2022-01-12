e20bf21a8810bc42989175f2f83104631f3d3437
Luke Collins <luke.a.collins@outlook.com>
2022-01-12 08:56:28 -0800

    feat: add function shells

---
+++ src/CanadaCovidMetrics/CanadaCovidMetrics.py
@@ -0,0 +1,19 @@
+from datetime import date
+
+today = '{}'.format(date.today())
+
+def total_cumulative_cases(loc='prov', date=None, after='2020-01-01', before=today):
+    return
+
+
+def total_cumulative_deaths(loc='prov', date=None, after='2020-01-01', before=today):
+    return
+
+
+def total_cumulative_recovered_cases(loc='prov', date=None, after='2020-01-01', before=today):
+    return
+
+
+def total_cumulative_vaccine_completion(loc='prov', date=None, after='2020-01-01', before=today):
+    return
+
