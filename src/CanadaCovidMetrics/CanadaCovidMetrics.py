1fafb5332987578eb63e941c7e2f1efd3afac165
Luke Collins <luke.a.collins@outlook.com>
2022-01-19 15:29:35 -0800

    feat: Add cumulative cases recovered function body
    Added function body to total_cumulative_recovered_cases.
    Removed unused code.

--- src/CanadaCovidMetrics/CanadaCovidMetrics.py
+++ src/CanadaCovidMetrics/CanadaCovidMetrics.py
@@ -1,21 +1,12 @@
-from datetime import date
+import datetime as dt
+from multiprocessing.sharedctypes import Value
 import requests
 import pandas as pd
 
-"""
-Sample format of a request 
-
-url = 'https://api.opencovid.ca/timeseries?stat=cases&loc=prov&date=01-09-2020'
-r = requests.get(url = url)
-json_body = r.json()
-
-convert to pd.DataFrame (optional?)
-
-    - If we do this, we'll have to update the docstrings
-    to specify that the functions return a pandas dataframe
-"""
-
-today = '{}'.format(date.today())
+today = '{}'.format(dt.date.today())
+format_ymd = "%Y-%m-%d"
+format_dmy = "%d-%m-%Y"
+format_prov = ['canada', 'prov', 'BC', 'AB', 'SK', 'MB', 'ON', 'QC', 'NL', 'NB', 'NS', 'PE', 'NT', 'YT', 'NU', 'RP']
 
 def total_cumulative_cases(loc='prov', date=None, after='2020-01-01', before=today):
     """Query total cumulative cases with ability to specify \
@@ -99,15 +90,51 @@ def total_cumulative_recovered_cases(loc='prov', date=None, after='2020-01-01', 
 
     Returns
     -------
-    dict
-        JSON response from queried api.
+    df
+        Pandas dataframe containing content of API response.
 
     Examples
     --------
     >>> total_cumulative_recovered_cases(loc='BC')
-    """
+    """  
+    if loc not in format_prov:
+        raise(ValueError("Value passed for loc argument is not recognized. Must be one of: 'prov', 'canada', or a two-letter capitalized province code"))
 
-    return
+    if date is not None:
+        try:
+            dt.datetime.strptime(date, format_ymd)
+        except:
+            try:
+                dt.datetime.strptime(date, format_dmy)
+            except:
+                raise(ValueError("Incorrect date format {} : date format should be either YYYY-MM-DD or DD-MM-YYYY".format(date)))
+        
+        url = 'https://api.opencovid.ca/timeseries?stat=recovered&loc={}&date={}'.format(loc, date)
+    
+    else:
+        try:
+            dt.datetime.strptime(before, format_ymd)
+        except:
+            try:
+                dt.datetime.strptime(before, format_dmy)
+            except:
+                raise(ValueError("Incorrect before-date format {} : date format should be either YYYY-MM-DD or DD-MM-YYYY".format(before)))
+        
+        try:
+            dt.datetime.strptime(after, format_ymd)
+        except:
+            try:
+                dt.datetime.strptime(after, format_dmy)
+            except:
+                raise(ValueError("Incorrect after-date format {} : date format should be either YYYY-MM-DD or DD-MM-YYYY".format(after)))
+
+        url = 'https://api.opencovid.ca/timeseries?stat=recovered&loc={}&after={}&before={}'.format(loc, after, before)
+    
+    r = requests.get(url = url)
+    json_body = r.json()['recovered']
+    df = pd.DataFrame.from_dict(json_body)
+
+    return df
 
 
 def total_cumulative_vaccine_completion(loc='prov', date=None, after='2020-01-01', before=today):
@@ -140,3 +167,4 @@ def total_cumulative_vaccine_completion(loc='prov', date=None, after='2020-01-01
 
     return
 
+print(total_cumulative_recovered_cases())
