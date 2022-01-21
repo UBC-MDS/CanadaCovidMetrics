8bb7e8de80522cc9d94e27814bf631cf9221a9c2
Bran Lam <76808779+ming0701@users.noreply.github.com>
2022-01-20 19:19:00 -0800

    Update test_total_cumulative_deaths

--- tests/test_CanadaCovidMetrics.py
+++ tests/test_CanadaCovidMetrics.py
@@ -60,10 +60,9 @@ def test_total_cumulative_deaths():
     """Test total_cumulative_deaths"""
 
     # Test correct data is returned when date is specified
-    data = [[1078, '18-01-2021', 31, 'BC']]
-    df = pd.DataFrame(data, columns = ['cumulative_deaths', 'date_death_report', 'death', 'province'])
+    df = ccm.total_cumulative_deaths(loc='BC', date='2021-01-18')
     assert df['cumulative_deaths'].sum() == 1078
-    assert df['death'].sum() == 31
+    assert df['deaths'].sum() == 31
     
     # Test correct data is returned when date is not specified
     df = ccm.total_cumulative_deaths(loc='BC', date=None, after='2020-01-01', before = '2020-07-30')
