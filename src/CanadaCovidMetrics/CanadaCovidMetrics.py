e3aec8548ba4abfda140d5e842623cbf3bbbe470
Adam Morphy <adam.morphy@outlook.com>
2022-01-21 18:14:13 -0800

    fix: merge conflict resolved

--- src/CanadaCovidMetrics/CanadaCovidMetrics.py
+++ src/CanadaCovidMetrics/CanadaCovidMetrics.py
@@ -7,16 +7,13 @@ today = '{}'.format(dt.date.today())
 
 def date_format_check(datestr):
     """Check date format is compatible with API call
-
     Parameters
     ----------
     datestr : str
         Date string to be checked
-
     Returns
     -------
     None
-
     Examples
     --------
     >>> date_format_check('2021-03-30')
@@ -41,11 +38,9 @@ def loc_format_check(locstr):
     ----------
     locstr : str
         Location string to be checked
-
     Returns
     -------
     None
-
     Examples
     --------
     >>> loc_format_check('2021-03-30')
@@ -59,7 +54,6 @@ def loc_format_check(locstr):
 def total_cumulative_cases(loc='prov', date=None, after='2020-01-01', before=today):
     """Query total cumulative cases with ability to specify \
         location and date range of returned data.
-
     Parameters
     ----------
     loc : str
@@ -73,19 +67,30 @@ def total_cumulative_cases(loc='prov', date=None, after='2020-01-01', before=tod
         Return data on and after the specified date YYYY-MM-DD.
     before : str
         Return data on and before the specified date YYYY-MM-DD.
-
     Returns
     -------
     dict
         JSON response from queried api.
-
     Examples
     --------
     >>> total_cumulative_cases(loc='BC')
     """
+     
+    loc_format_check(loc)  # check location is valid
 
-    return
+    if date is not None:
+        date_format_check(date)  # check date is valid
+        url = 'https://api.opencovid.ca/timeseries?stat=cases&loc={}&date={}'.format(loc, date) 
+    else:
+        date_format_check(before)  # check before-date is valid
+        date_format_check(after)  # check after-date is valid
+        url = 'https://api.opencovid.ca/timeseries?stat=cases&loc={}&after={}&before={}'.format(loc, after, before)
+    
+    r = requests.get(url = url)
+    json_body = r.json()['cases']
+    df = pd.DataFrame.from_dict(json_body)
 
+    return df
 
 def total_cumulative_deaths(loc='prov', date=None, after='2020-01-01', before=today):
     """Query total cumulative deaths with ability to specify \
@@ -132,7 +137,6 @@ def total_cumulative_deaths(loc='prov', date=None, after='2020-01-01', before=to
 def total_cumulative_recovered_cases(loc='prov', date=None, after='2020-01-01', before=today):
     """Query total cumulative recovered cases with ability \
         to specify location and date range of returned data.
-
     Parameters
     ----------
     loc : str
@@ -146,12 +150,10 @@ def total_cumulative_recovered_cases(loc='prov', date=None, after='2020-01-01', 
         Return data on and after the specified date YYYY-MM-DD.
     before : str
         Return data on and before the specified date YYYY-MM-DD.
-
     Returns
     -------
     df
         Pandas dataframe containing content of API response.
-
     Examples
     --------
     >>> total_cumulative_recovered_cases(loc='BC')
--- tests/test_CanadaCovidMetrics.py
+++ tests/test_CanadaCovidMetrics.py
@@ -1,25 +1,103 @@
-from CanadaCovidMetrics import CanadaCovidMetrics
-import datetime as dt
-from multiprocessing.sharedctypes import Value
-import requests
+from CanadaCovidMetrics import CanadaCovidMetrics as ccm
+import pytest
 import pandas as pd
+
+def test_date_format_check():
+    """Test date_format_check"""
+
+    # initialise values to test
+    date_correct_format = "2021-10-31"
+    date_wrong_format = "10-31-2021"
+    not_a_date = "121242539"
+
+    # expect None returned for proper date format
+    assert ccm.date_format_check(date_correct_format) == None, "Expect None returned for proper date formate failed"
+    
+    # expect ValueError raised for incorrect date format
+    with pytest.raises(ValueError):
+        ccm.date_format_check(date_wrong_format)
+    
+    with pytest.raises(ValueError):
+        ccm.date_format_check(not_a_date)
+
+def test_loc_format_check():
+    """Test loc_format_check"""
+
+    # initialise values to test
+    loc_correct_format = "canada"
+    loc_wrong_format = "PEI"
+    not_a_loc = "ABC1234!"
+
+    # expect None returned for proper loc format
+    assert ccm.loc_format_check(loc_correct_format) == None, "Expect None returned for proper loc formate failed"
+    
+    # expect ValueError raised for incorrect loc format
+    with pytest.raises(ValueError):
+        ccm.loc_format_check(loc_wrong_format)
+    
+    with pytest.raises(ValueError):
+        ccm.loc_format_check(not_a_loc)
+
+def test_total_cumulative_recovered_cases():
+    """Test loc_format_check"""
+
+    # check default call returns a dataframe object
+    assert isinstance(ccm.total_cumulative_recovered_cases(), pd.DataFrame), "Expect dataframe object return failed"
+
+    # check default call returns non-empty dataframe object
+    assert ccm.total_cumulative_recovered_cases().empty == False, "Expect non-empty dataframe return failed"
+    
+    # check default call returns expected dataframe components
+    assert (ccm.total_cumulative_recovered_cases().columns == [
+        'cumulative_recovered',
+        'date_recovered',
+        'province',
+        'recovered'
+        ]).all(), "Expect correct dataframe columns return failed"
+
 
 def test_total_cumulative_deaths():
     """Test total_cumulative_deaths"""
 
     # Test correct data is returned when date is specified
-    data = [[1078, '18-01-2021', 31, 'BC']]
-    df = pd.DataFrame(data, columns = ['cumulative_deaths', 'date_death_report', 'death', 'province'])
+    df = ccm.total_cumulative_deaths(loc='BC', date='2021-01-18')
     assert df['cumulative_deaths'].sum() == 1078
-    assert df['death'].sum() == 31
+    assert df['deaths'].sum() == 31
     
     # Test correct data is returned when date is not specified
-    df = total_cumulative_deaths(loc='BC', date=None, after='2020-01-01', before = '2020-07-30')
+    df = ccm.total_cumulative_deaths(loc='BC', date=None, after='2020-01-01', before = '2020-07-30')
     assert df['deaths'].sum() == 194
 
     # Test correct data size is returned
-    df = total_cumulative_deaths(loc='BC', date=None, after='2020-01-01', before = '2020-10-30')
+    df = ccm.total_cumulative_deaths(loc='BC', date=None, after='2020-01-01', before = '2020-10-30')
     assert df.shape == (237, 4)
+
+
+def test_total_cummulative_cases():
+    """ Test total_cummulative_cases"""
+    
+    # Test correct data is returned when date is specified
+    data = [[17, 56, "25-01-2020", "Alberta"]]
+    df = ccm.total_cumulative_cases(loc='AB', date='15-03-2020', after='2020-01-01', before=today)
+    assert df['cumulative_cases'].sum() == 56, 'Incorrect data obtained!'
+    assert df['cases'].sum() == 17, 'Incorrect data obtained!'
+    
+    #Test correct data is returned when date is not specified
+    data = [[0, 0 ,'25-01-2020',"Alberta"]]
+    df = pd.DataFrame(data, columns = ["cases", "cumulative_cases", "date_report", "province"])
+    assert df['cumulative_cases'].sum() == 0, 'Incorrect data obtained!'
+    assert df['cases'].sum() == 0, 'Incorrect data obtained!'
+    
+    
+    #Test to check if every province data is returned for a given day when loc = "prov"
+    
+    df = ccm.total_cumulative_cases(loc='prov', date='15-03-2020', after='2020-01-01', before=today)
+    assert df["province"].nunique() == 14, 'Incorrect data obtained!'
+    
+    # Test to check for the correct columns in the dataframe returned
+    
+    df = ccm.total_cumulative_cases(loc='AB', date='15-03-2020', after='2020-01-01', before=today)
+    assert (df.columns == ['cases', 'cumulative_cases', 'date_report', 'province']).all(),  'Incorrect data obtained!'
 
 
 def test_total_cumulative_vaccine_completion():
